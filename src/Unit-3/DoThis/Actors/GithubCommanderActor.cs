ec014632d81d979357a7d37b1f5cae57baf49d55
Steve Costa <stevencosta@gmail.com>
2016-08-07 16:50:00 -0400

    Completed akka.net unit 3 tutorial

--- src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
+++ src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
@@ -8,7 +8,7 @@ namespace GithubActors.Actors
     /// <summary>
     /// Top-level actor responsible for coordinating and launching repo-processing jobs
     /// </summary>
-    public class GithubCommanderActor : ReceiveActor
+    public class GithubCommanderActor : ReceiveActor, IWithUnboundedStash
     {
         #region Message classes
 
@@ -46,35 +46,87 @@ namespace GithubActors.Actors
 
         private IActorRef _coordinator;
         private IActorRef _canAcceptJobSender;
+        private int pendingJobReplies;
+        private RepoKey _repoJob;
 
         public GithubCommanderActor()
         {
+            Ready();
+        }
+
+        private void Ready()
+        {
             Receive<CanAcceptJob>(job =>
             {
-                _canAcceptJobSender = Sender;
                 _coordinator.Tell(job);
+                _repoJob = job.Repo;
+                BecomeAsking();
+            });
+        }
+
+        private void BecomeAsking()
+        {
+            _canAcceptJobSender = Sender;
+            pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees())
+                .Result.Members.Count();
+            Become(Asking);
+
+            // Send ourselves a receive timeout message if no message within 3 seconds
+            Context.SetReceiveTimeout(TimeSpan.FromSeconds(3));
+        }
+
+        private void Asking()
+        {
+            // Stash any subsequent requests
+            Receive<CanAcceptJob>(job => Stash.Stash());
+
+            Receive<ReceiveTimeout>(timeout =>
+            {
+                _canAcceptJobSender.Tell(new UnableToAcceptJob(_repoJob));
+                BecomeReady();
             });
 
             Receive<UnableToAcceptJob>(job =>
             {
-                _canAcceptJobSender.Tell(job);
+                pendingJobReplies--;
+                if (pendingJobReplies == 0)
+                {
+                    _canAcceptJobSender.Tell(job);
+                    BecomeReady();
+                }
             });
 
             Receive<AbleToAcceptJob>(job =>
             {
                 _canAcceptJobSender.Tell(job);
 
-                //start processing messages
-                _coordinator.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
+                // Start processing messages
+                Sender.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
 
-                //launch the new window to view results of the processing
-                Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
+                // Launch the new window to view processing results
+                Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(
+                    new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
+
+                BecomeReady();
             });
+        }
+
+        private void BecomeReady()
+        {
+            Become(Ready);
+            Stash.UnstashAll();
+
+            // Cancel ReceiveTimeout
+            Context.SetReceiveTimeout(null);
         }
 
         protected override void PreStart()
         {
-            _coordinator = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name);
+            // Create a broadcast router that will ask all of them if they are available to work
+            _coordinator =
+                Context.ActorOf(Props.Create(() => new GithubCoordinatorActor())
+                    .WithRouter(FromConfig.Instance),
+                    ActorPaths.GithubCoordinatorActor.Name);
             base.PreStart();
         }
 
@@ -84,5 +136,7 @@ namespace GithubActors.Actors
             _coordinator.Tell(PoisonPill.Instance);
             base.PreRestart(reason, message);
         }
+
+        public IStash Stash { get; set; }
     }
 }
